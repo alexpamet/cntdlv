ea15f4e57c78e03b376b209017aea0f3a81d4fb6
aca <aleksandar.zlatkovic@vicert.com>
2015-11-23 15:51:15 +0100

    Lambda example.

---
+++ lambda/src/main/java/com/vicert/aws/lambda/example/Hello.java
@@ -0,0 +1,13 @@
+package com.vicert.aws.lambda.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+public class Hello {
+
+	public String myHandler(int myCount, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("received : " + myCount);
+        return String.valueOf(myCount);
+    }
+}
