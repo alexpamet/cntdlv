5bb7ef31f39e936a10f78cca09286477dc384e2c
aca <aleksandar.zlatkovic@vicert.com>
2015-12-04 14:49:01 +0100

    Tasks.

---
+++ aws/jenkins/jenkins-launcher.js
@@ -0,0 +1,18 @@
+var aws = require('aws-sdk');
+var ecs = new aws.ECS({apiVersion: '2014-11-13'});
+
+exports.handler = function(event, context) {
+    console.log('Received event:');
+    console.log(JSON.stringify(event, null, '  '));
+
+	var params = {
+		taskDefinition: "jenkinstaskdefinition",
+		count: 1
+	};	
+	
+	ecs.runTask(params, function (err, data) {
+		if (err) { console.warn('error: ', "Error while starting task: " + err); }
+		else { console.info('Task ' + config.task + ' started: ' + JSON.stringify(data.tasks))}
+	});
+
+};
