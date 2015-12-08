b56bb5dc824473749fa24a589c70752167a2ef55
aca <aleksandar.zlatkovic@vicert.com>
2015-12-08 14:17:36 +0100

    Started creation of the production version

---
+++ solution/docker/images/node-aws-slack-bridge/server.js
@@ -0,0 +1,87 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var request = require('request');
+
+// Application port
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// URL for AWS API
+var awsProtocol = "http://"
+var awsHost = "aca-hp:8080/api/";
+
+// setup router
+router
+    .get('/start', function(req, res) {
+  
+		console.log('Starting Jenkins');
+
+		startInstance('jenkins');
+		
+		res.setHeader('Content-Type', 'application/json');
+		res.json(JSON.stringify({ instance: 'jenkins', status: 'MOCK'}));   
+    })
+    ;
+
+// Function that calls start instance api for an instance
+function startInstance(instance) {
+	performRequest('/start', {
+		instance: instance
+	}, function (error, response, body) {
+		console.log('Start instance response body', body);
+		
+		var interval = setInterval(function () {
+			performRequest('/test', {
+				instance: instance
+				}, function (error, response, body) {
+//					var responseObject = JSON.parse(data);
+					console.log(body);
+					console.log('Test instance ' + body.instance + ' state: ' + body.started);
+					if (body.started == true) {
+					clearInterval(interval);
+				}
+			});
+		}, 1000);
+		
+	});
+}
+
+// Function that performs JSON request
+function performRequest(endpoint, data, success) {
+	var dataString = JSON.stringify(data);
+	var headers = {};
+  
+	console.log('Instance: ', data.instance);
+	
+	request (
+		{
+			url: awsProtocol + awsHost + endpoint,
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Content-Length': dataString.length
+			},
+			json: {
+				instance: data.instance
+			}
+		}, success
+	);
+}
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
