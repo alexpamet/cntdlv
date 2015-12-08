83fa816274d0e649f0ed49aaeb3a9e0f63ced4de
alexpamet <aleksandar.zlatkovic@vicert.com>
2015-12-08 10:53:05 +0100

    small fix

--- node/agent/server.js
+++ node/agent/server.js
@@ -8,7 +8,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var request = require('request');
 
-var port = process.env.PORT || 80;        // set our port
+var port = process.env.PORT || 8081;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
